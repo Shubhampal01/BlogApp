653612f97924e1c9acefcf3b3432efe8ed381fcf
Shubhampal01 <shubhampal1002@gmail.com>
2024-08-30 22:17:55 +0530

    signup comp completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ import {useForm} from 'react-hook-form'
 function Login() {
     const navigate = useNavigate();
     const diapatch = useDispatch();
-    const {register,useRegister} = useForm();
+    const {register,handleSubmit} = useForm();
     const [error,setError] = useState("");
     const login = async(data)=>{
         setError("");
@@ -33,18 +33,18 @@ function Login() {
         rounded-xl p-10 border border-black/10`}
         >
             <div className="mb-2 flex justify-center">
-                <span className="inline-block w-full max-w[100px]">
+                <span className="inline-block w-full max-w-[100px]">
                     <Logo width='100%'/>
                 </span>
             </div>
             <h2 className="text-center text-2xl font-bold leading-tight">
-                Sign in to yuor account
+                Sign in to your account
             </h2>
             <p className="mt-2 text-center text-base text-black/60">
                 Don&apos;t have any account?&nbsp;
                 <Link
                 to='/signup'
-                className='font-medium text-primary 
+                className='font-medium text-blue-600
                 transition-all duration-200 hover:underline'
                 >
                     SignUp
@@ -52,6 +52,35 @@ function Login() {
             </p>
             {error && <p className='text-red-600 mt-8 text-center'>
                 {error}</p>}
+            <form onSubmit={handleSubmit(login)}
+                className = 'mt-8'>
+                    <div className="space-y-5">
+                        <Input
+                            label = "Email: "
+                            placeholder = "Enter your email"
+                            type = "email"
+                            {...register("email",{
+                                required:true,
+                                validate:{
+                                    matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                    "Email address must be a valid address",
+                                }
+                            })}  
+                        />
+                        <Input
+                            label = "Password: "
+                            type = "password"
+                            placeholder = "Enter your password"
+                            {...register("password",{
+                                required:true,
+                            })}
+                        />
+                        <Button
+                            type='submit'
+                            className='w-full'
+                        >Sign in</Button>
+                    </div>
+            </form>
         </div>
     </div>
   )
